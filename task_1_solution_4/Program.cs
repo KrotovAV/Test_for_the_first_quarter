90046f013241f9e5e091631a674be1a5297078fb
KrotovAV <KrotovAV@tut.by>
2022-09-06 13:00:14 +0300

    start_sol4done

--- task_1_solution_4/Program.cs
+++ task_1_solution_4/Program.cs
@@ -8,5 +8,61 @@
 //элемнтов массива и потом каждый элемент".
 
 
+string[] WriteReadLine(int n, string[] arr3)
+{
+    for (int i = 0; i < n; i++)
+    {
+    Console.WriteLine("Write array element");
+    arr3[i] = (Console.ReadLine());
+    }
+    return arr3;
+}
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write($"{arr[i]}, ");
+        //Console.WriteLine();
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+string[] NewArrThreeSymbol(string[] arr2)
+{
+    int n = 1;
+    string[] newArr = new string[n];
+    for (int i = 0; i < arr2.Length; i++)
+    {
+        if (arr2[i].Length <= 3) 
+        {
+            Array.Resize(ref newArr, n+1);
+            newArr[n-1] = arr2[i];
+//            Console.WriteLine($" {i} - {n-1}");
+            n++;
+        }
+    }
+    Array.Resize(ref newArr, n-1);
+    return newArr;
+}
+
+Console.WriteLine();
+Console.WriteLine("Write array size");
+int n = int.Parse(Console.ReadLine());
+
+string[] arr = new string[n];
+
+WriteReadLine(n, arr);
+
+Console.WriteLine();
+Console.Write("Source array - ");
+
+PrintArray(arr);
+
+string[] new3s = NewArrThreeSymbol(arr);
+Console.Write("Final array - ");
+PrintArray(new3s);
+
 
 
